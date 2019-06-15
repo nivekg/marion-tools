6baeaaa61c4c45989ec7d6d366a68e569b2e1211
nivekg <nivekghazi@outlook.com>
2019-06-15 23:01:11 +0200

    removed hardcoded channels and bytes_per_packet

--- albatrostools.py
+++ albatrostools.py
@@ -31,7 +31,7 @@ def unpack_2_bit(data, num_channels):
     real_pol0=numpy.asarray(numpy.right_shift(numpy.bitwise_and(data, 0xC0), 6), dtype="int8")
     imag_pol0=numpy.asarray(numpy.right_shift(numpy.bitwise_and(data, 0x30), 4), dtype="int8")
     real_pol1=numpy.asarray(numpy.right_shift(numpy.bitwise_and(data, 0x0C), 2), dtype="int8")
-    imag_pol0=numpy.asarray(numpy.bitwise_and(data, 0x03), dtype="int8")
+    imag_pol1=numpy.asarray(numpy.bitwise_and(data, 0x03), dtype="int8")
 
     real_pol0[real_pol0<=1]=real_pol0[real_pol0<=1]-2
     real_pol0[real_pol0>=2]=real_pol0[real_pol0>=2]-1
@@ -53,21 +53,23 @@ def unpack_2_bit(data, num_channels):
     del imag_pol1
     pol0=pol0.reshape(-1, num_channels)
     pol1=pol1.reshape(-1, num_channels)
+    print(pol0.shape)
+    print(pol1.shape)
     return pol0, pol1
     
 def unpack_4_bit(data, num_channels):
     pol0_bytes=data[:, 0::2]
     pol1_bytes=data[:, 1::2]
-    real_pol0=numpy.right_shift(numpy.bitwise_and(pol0_bytes, 0xf0), 4)
-    imag_pol0=numpy.bitwise_and(pol0_bytes, 0x0f)
-    real_pol1=numpy.right_shift(numpy.bitwise_and(pol1_bytes, 0xf0), 4)
-    imag_pol1=numpy.bitwise_and(pol1_bytes, 0x0f)
+    real_pol0=numpy.asarray(numpy.right_shift(numpy.bitwise_and(pol0_bytes, 0xf0), 4), dtype="int8")
+    imag_pol0=numpy.asarray(numpy.bitwise_and(pol0_bytes, 0x0f), dtype="int8")
+    real_pol1=numpy.asarray(numpy.right_shift(numpy.bitwise_and(pol1_bytes, 0xf0), 4), dtype="int8")
+    imag_pol1=numpy.asarray(numpy.bitwise_and(pol1_bytes, 0x0f), dtype="int8")
     
-    real_pol0[real_pol0>7]=real_pol0[real_pol0>7]-16
-    imag_pol0[imag_pol0>7]=imag_pol0[imag_pol0>7]-16
+    real_pol0[real_pol0>8]=real_pol0[real_pol0>8]-16
+    imag_pol0[imag_pol0>8]=imag_pol0[imag_pol0>8]-16
 
-    real_pol1[real_pol1>7]=real_pol1[real_pol1>7]-16
-    imag_pol1[imag_pol1>7]=imag_pol1[imag_pol1>7]-16
+    real_pol1[real_pol1>8]=real_pol1[real_pol1>8]-16
+    imag_pol1[imag_pol1>8]=imag_pol1[imag_pol1>8]-16
 
     pol0=real_pol0+1J*imag_pol0
     pol1=real_pol1+1J*imag_pol1
@@ -81,9 +83,9 @@ def unpack_4_bit(data, num_channels):
     pol1=pol1.reshape(-1, num_channels)
     return pol0, pol1
     
-def get_data(file_name):
+def get_data(file_name, channels, bytes_per_packet):
     file_data=open(file_name, "r")
-    data=numpy.fromfile(file_data, dtype=[("spec_num", ">I"), ("spectra", "1230B")])
+    data=numpy.fromfile(file_data, dtype=[("spec_num", ">I"), ("spectra", "%dB"%(bytes_per_packet-4))])
     file_data.close()
-    spectra=data["spectra"].reshape(-1, 246)
+    spectra=data["spectra"].reshape(-1, channels)
     return data["spec_num"], spectra
