7d67280a2e03e0703c814567faca226b1eb12b4c
nivekg <nivekghazi@outlook.com>
2019-06-15 23:00:14 +0200

    added channels and bytes_per_packet and command line options

--- plot_4_bit_hist.py
+++ plot_4_bit_hist.py
@@ -9,36 +9,39 @@ def plot_4_bit_hist(pol0, pol1, channel):
     print("plotting channel "+str(channel))
     fig=pyplot.figure(figsize=(12, 10))
     axs1=fig.add_subplot(221)
-    axs1.hist(numpy.real(pol0), bins=numpy.arange(-7, 8, 1), edgecolor='black', linewidth=1.2, align="left")
+    axs1.hist(numpy.real(pol0), bins=numpy.arange(-7, 9, 1), edgecolor='black', linewidth=1.2, align="left")
     axs1.title.set_text("Real Pol0")
-    #axs1.grid()
+    axs1.set_xticks(range(-8, 9, 1))
+    axs1.set_xticklabels(range(-8, 9, 1))
     axs2=fig.add_subplot(222)
-    axs2.hist(numpy.imag(pol0), bins=numpy.arange(-7, 8, 1), edgecolor='black', linewidth=1.2, align="left")
+    axs2.hist(numpy.imag(pol0), bins=numpy.arange(-7, 9, 1), edgecolor='black', linewidth=1.2, align="left")
     axs2.title.set_text("Imag Pol0")
-    #axs2.grid()
+    axs2.set_xticks(range(-8, 9, 1))
+    axs2.set_xticklabels(range(-8, 9, 1))
     axs3=fig.add_subplot(223)
-    axs3.hist(numpy.real(pol1), bins=numpy.arange(-7, 8, 1), edgecolor='black', linewidth=1.2, align="left")
+    axs3.hist(numpy.real(pol1), bins=numpy.arange(-7, 9, 1), edgecolor='black', linewidth=1.2, align="left")
     axs3.title.set_text("Real Pol1")
-    #axs3.grid()
+    axs3.set_xticks(range(-8, 9, 1))
+    axs3.set_xticklabels(range(-8, 9, 1))
     axs4=fig.add_subplot(224)
-    axs4.hist(numpy.imag(pol1), bins=numpy.arange(-7, 8, 1), edgecolor='black', linewidth=1.2, align="left")
+    axs4.hist(numpy.imag(pol1), bins=numpy.arange(-7, 9, 1), edgecolor='black', linewidth=1.2, align="left")
     axs4.title.set_text("Imag Pol1")
-    #axs4.grid()
+    axs4.set_xticks(range(-8, 9, 1))
+    axs4.set_xticklabels(range(-8, 9, 1))
     pyplot.savefig("chan_"+str(channel)+"_4_bit_hist.png")
     pyplot.clf()
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("datafile", type=str, help="data file for histrogram")
+    parser.add_argument("-c", "--channels", type=int, help="Number of channels")
+    parser.add_argument("-b", "--bytes_per_packet", type=int, help="NUmber of bytes per packet")
     args=parser.parse_args()
-    
-    channels=20
 
-    spec_num, data=albatrostools.get_data(args.datafile)
-    print(data.shape)
-    pol0, pol1=albatrostools.unpack_4_bit(data, channels)
+    spec_num, data=albatrostools.get_data(args.datafile, args.channels, args.bytes_per_packet)
+    pol0, pol1=albatrostools.unpack_4_bit(data, args.channels)
     
-    for i in range(channels):
+    for i in range(args.channels):
         plot_4_bit_hist(pol0[:, i], pol1[:, 1], i)
 
         
