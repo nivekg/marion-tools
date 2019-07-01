9b1aee1a72384f8507632ad934602de9e2e35882
nivekg <nivekghazi94@gmail.com>
2019-07-01 11:10:12 +0200

    bug fix: I was incorrectly appending channels in one bit unpack

--- albatrostools.py
+++ albatrostools.py
@@ -8,15 +8,15 @@ def unpack_1_bit(data, num_channels):
     real_pol0_chan1=numpy.asarray(numpy.right_shift(numpy.bitwise_and(data, 0x08), 3), dtype="int8")
     imag_pol0_chan1=numpy.asarray(numpy.right_shift(numpy.bitwise_and(data, 0x04), 2), dtype="int8")
     real_pol1_chan1=numpy.asarray(numpy.right_shift(numpy.bitwise_and(data, 0x02), 1), dtype="int8")
-    imag_pol1_chan1=numpy.right_shift(numpy.bitwise_and(data, 0x01), dtype="int8")
-    real_pol0=numpy.append(real_pol0_chan0, real_pol0_chan1)
-    imag_pol0=numpy.append(imag_pol0_chan0, imag_pol0_chan1)
-    real_pol1=numpy.append(real_pol1_chan0, real_pol1_chan1)
-    imag_pol1=numpy.append(imag_pol1_chan0, imag_pol1_chan1)
+    imag_pol1_chan1=numpy.asarray(numpy.bitwise_and(data, 0x01), dtype="int8")
+    real_pol0=numpy.ravel(numpy.column_stack((real_pol0_chan0, real_pol0_chan1)))
+    imag_pol0=numpy.ravel(numpy.column_stack((imag_pol0_chan0, imag_pol0_chan1)))
+    real_pol1=numpy.ravel(numpy.column_stack((real_pol1_chan0, real_pol1_chan1)))
+    imag_pol1=numpy.ravel(numpy.column_stack((imag_pol1_chan0, imag_pol1_chan1)))
     real_pol0[real_pol0==0]=-1
     imag_pol0[imag_pol0==0]=-1
-    real_pol0[real_pol1==0]=-1
-    imag_pol0[imag_pol1==0]=-1
+    real_pol1[real_pol1==0]=-1
+    imag_pol1[imag_pol1==0]=-1
     pol0=real_pol0+1J*imag_pol0
     pol1=real_pol1+1J*imag_pol1
     del real_pol0
@@ -82,10 +82,19 @@ def unpack_4_bit(data, num_channels):
     pol0=pol0.reshape(-1, num_channels)
     pol1=pol1.reshape(-1, num_channels)
     return pol0, pol1
+
+def correlate(pol0, pol1):
+    pols={}
+    data=[pol0, pol1]
+    for i in range(2):
+        for j in range(i, 2):
+            pols["pol%d%d"%(i, j)]=data[i]*numpy.conj(data[j])
+    return pols
     
-def get_data(file_name, channels, bytes_per_packet):
+def get_data(file_name, channels, bytes_per_packet, items=-1):
     file_data=open(file_name, "r")
-    data=numpy.fromfile(file_data, dtype=[("spec_num", ">I"), ("spectra", "%dB"%(bytes_per_packet-4))])
+    data=numpy.fromfile(file_data, count=items, dtype=[("spec_num", ">I"), ("spectra", "%dB"%(bytes_per_packet-4))])
     file_data.close()
+    #mem_data=numpy.memmap(data, dtype=[("spec_num", ">I"), ("spectra", "%dB"%(bytes_per_packet-4))])
     spectra=data["spectra"].reshape(-1, channels)
     return data["spec_num"], spectra
